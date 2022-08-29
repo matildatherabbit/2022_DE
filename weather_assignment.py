f4a060de92d9e7cc7d3aeffbe1d8588d93f4c340
Juhye Kim <helennearing@gmail.com>
2022-08-29 16:20:24 +0900

    complete_first

--- weather_assignment.py
+++ weather_assignment.py
@@ -19,91 +19,95 @@ def get_Redshift_connection(autocommit=False):
     conn.autocommit = autocommit
     return conn.cursor()
 
-
-def extract(**context):
-    link = context["params"]["api"]
-    task_instance = context['task_instance']
-    execution_date = context['execution_date']
-
-    logging.info(execution_date)
-    f = requests.get(link)
-    f_js = f.json()
-    logging.info(f_js)
-    return (f_js)
-
-def transform(**context):
-    content = context["task_instance"].xcom_pull(key="return_value", task_ids="extract")
-    weather_info = content.loads(f_js)
-    return weather_info
-    logging.info(weather_info)
-
-
-def load(**context):
+def etl(**context):
     schema = context["params"]["schema"]
     table = context["params"]["table"]
+    lat = context["params"]["lat"]
+    lon = context["params"]["lon"]
+    api_key = Variable.get("open_weather_api_key")
+
+    url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={api_key}&units=metric&exclude=current,minutely,hourly,alerts"
+    response = requests.get(url)
+    data = json.loads(response.text)
+
+    """
+    {'dt': 1622948400, 'sunrise': 1622923873, 'sunset': 1622976631, 'moonrise': 1622915520, 'moonset': 1622962620, 'moon_phase': 0.87, 'temp': {'day': 26.59, 'min': 15.67, 'max': 28.11, 'night': 22.68, 'eve': 26.29, 'morn': 15.67}, 'feels_like': {'day': 26.59, 'night': 22.2, 'eve': 26.29, 'morn': 15.36}, 'pressure': 1003, 'humidity': 30, 'dew_point': 7.56, 'wind_speed': 4.05, 'wind_deg': 250, 'wind_gust': 9.2, 'weather': [{'id': 802, 'main': 'Clouds', 'description': 'scattered clouds', 'icon': '03d'}], 'clouds': 44, 'pop': 0, 'uvi': 3}
+    """
+    ret = []
+    for d in data["daily"]:
+        day = datetime.fromtimestamp(d["dt"]).strftime('%Y-%m-%d')
+        ret.append("('{}',{},{},{})".format(day, d["temp"]["day"], d["temp"]["min"], d["temp"]["max"]))
 
     cur = get_Redshift_connection()
-    weather_info = context["task_instance"].xcom_pull(key="return_value", task_ids="transform")
-    main_table = '''CREATE TABLE helennearing.weather_forecast (
-                date date primary key,
-                temp float,
-                min_temp float,
-                max_temp float,
-                created_date timestamp default GETDATE())'''
-    for date, temp, min_temp, max_temp in weather_info['daily']:
-        print(date['dt'], temp['day'], min_temp['min'], max_temp['max'])
-        sql += f"""INSERT INTO {schema}.{table} VALUES  ('{date}, '{temp}', '{min}', '{max}');"""
-    sql += "END;"
-    logging.info(sql)
-    cur.execute(main_table)
-#     temp_table = 
-
-# def reload(**context):
-#     sql = "BEGIN; DELETE FROM {schema}.{table};".format(schema=schema, table=table)
     
-#     logging.info(sql)
-#     cur.execute(sql)
+    # 임시 테이블 생성
+    create_sql = f"""DROP TABLE IF EXISTS {schema}.temp_{table};
+    CREATE TABLE {schema}.temp_{table} (LIKE {schema}.{table} INCLUDING DEFAULTS);INSERT INTO {schema}.temp_{table} SELECT * FROM {schema}.{table};"""
+    logging.info(create_sql)
+    try:
+        cur.execute(create_sql)
+        cur.execute("COMMIT;")
+    except Exception as e:
+        cur.execute("ROLLBACK;")
+        raise
 
-dag_weather_assignment = DAG(
+    # 임시 테이블 데이터 입력
+    insert_sql = f"INSERT INTO {schema}.temp_{table} VALUES " + ",".join(ret)
+    logging.info(insert_sql)
+    try:
+        cur.execute(insert_sql)
+        cur.execute("COMMIT;")
+    except Exception as e:
+        cur.execute("ROLLBACK;")
+        raise
+
+    # 기존 테이블 대체
+    alter_sql = f"""DELETE FROM {schema}.{table};
+      INSERT INTO {schema}.{table}
+      SELECT date, temp, min_temp, max_temp FROM (
+        SELECT *, ROW_NUMBER() OVER (PARTITION BY date ORDER BY updated_date DESC) seq
+        FROM {schema}.temp_{table}
+      )
+      WHERE seq = 1;"""
+    logging.info(alter_sql)
+    try:
+        cur.execute(alter_sql)
+        cur.execute("COMMIT;")
+    except Exception as e:
+        cur.execute("ROLLBACK;")
+        raise
+
+"""
+CREATE TABLE helennearing.weather_forecast (
+    date date,
+    temp float,
+    min_temp float,
+    max_temp float,
+    updated_date timestamp default GETDATE()
+);
+"""
+
+dag = DAG(
     dag_id = 'dag_weather_assignment',
-    start_date = datetime(2022,8,27), # 날짜가 미래인 경우 실행이 안됨
-    schedule_interval = '0 2 * * *',  # 적당히 조절
+    start_date = datetime(2022,8,24), # 날짜가 미래인 경우 실행이 안됨
+    schedule_interval = '0 4 * * *',  # 적당히 조절
     max_active_runs = 1,
     catchup = False,
     default_args = {
         'retries': 1,
         'retry_delay': timedelta(minutes=3),
-        # 'on_failure_callback': slack.on_failure_callback,
     }
 )
 
 
-extract = PythonOperator(
-    task_id = 'extract',
-    python_callable = extract,
+etl = PythonOperator(
+    task_id = 'etl',
+    python_callable = etl,
     params = {
-        'api':  Variable.get("open_weather_api_key")
-    },
-    provide_context=True,
-    dag = dag_weather_assignment)
-
-transform = PythonOperator(
-    task_id = 'transform',
-    python_callable = transform,
-    params = { 
-    },  
-    provide_context=True,
-    dag = dag_weather_assignment)
-
-load = PythonOperator(
-    task_id = 'load',
-    python_callable = load,
-    params = {
+        "lat": 37.5665,
+        "lon": 126.9780, 
         'schema': 'helennearing',   ## 자신의 스키마로 변경
         'table': 'weather_forecast'
     },
     provide_context=True,
     dag = dag_weather_assignment)
-
-extract >> transform >> load
-
